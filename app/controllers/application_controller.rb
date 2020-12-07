77870e4199b0babf680e6ddeb3aea8aaddcca100
Desiree Lerma <desi.lerma96@yahoo.com>
2020-12-06 23:16:59 -0600

    commit tree 91935c937b22768b45249eb205bb3dd619e839f3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,25 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+      configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions unless test?
+        set :session_secret, "secret"
+      end
+   
+      get '/' do
+        erb :index
+      end
 
+    helpers do
+
+      def logged_in?
+        !!current_user
+      end
+
+      def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'/users/new'
+    end
 
-end
+    post '/signup' do
+        user = User.create(email: params["email"], password: params["password"])
+            if user.email.blank? || user.password.blank? || User.find_by_email(params["email"])
+            redirect '/signup'
+            else
+            user.save
+            session[:user_id] = user.user_id
+            redirect '/tweets'
+        end
+    end
+
+    get '/login' do
+        erb :'/login'
+    end
+
+    post 'login' do
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.user_id
+            redirect '/tweets'
+        else 
+            redirect '/signup'
+        end 
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.destory
+            redirect '/login'
+        else
+            redirect to '/'
+        end
+    end
+
+ end
